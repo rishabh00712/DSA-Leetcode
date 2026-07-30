35b63038de3f1e6763f0931069f4e75b2618cc0b
Rishabh Garai <160108941+rishabh00712@users.noreply.github.com>
2026-07-30 11:48:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (77.73%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -3,15 +3,16 @@ public:
     int minimumPushes(string word) {
         int ans=0;
         for(int i=0;i<word.size();i++){
-            if(i>=0 && i<8){
-                ans++;
-            }else if(i>=8 && i<16){
-                ans+=2;
-            }else if(i>=16 && i<24){
-                ans+=3;
-            }else{
-                ans+=4;
-            }
+            // if(i>=0 && i<8){
+            //     ans++;
+            // }else if(i>=8 && i<16){
+            //     ans+=2;
+            // }else if(i>=16 && i<24){
+            //     ans+=3;
+            // }else{
+            //     ans+=4;
+            // }
+            ans+=(i/8+1);
         }
         return ans;
     }
