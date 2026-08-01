3be0035980386cebd2c8b734230395dd892b1fe4
Rishabh Garai <160108941+rishabh00712@users.noreply.github.com>
2026-08-01 20:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (45.39%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    bool dpFun(vector<int>& nums,int i,int j,int p1,int p2,bool isP1turn){
-        if(i>j){
-            return p1>=p2;
-        }
-        bool p11=false,p12=false,p21=false,p22=false;
-        bool actualP2=false;
-        if(isP1turn){
-            p11=dpFun(nums,i+1,j,p1+nums[i],p2,false);
-            p12=dpFun(nums,i,j-1,p1+nums[j],p2,false);
-        }else{
-            p21=dpFun(nums,i+1,j,p1,p2+nums[i],true);
-            p22=dpFun(nums,i,j-1,p1,p2+nums[j],true);
-            actualP2=p21 && p22;
-        }
-        return p11 || p12 || actualP2;
+    int solve(vector<int>& nums, int i, int j, vector<vector<int>>& dp) {
+        if (i == j) return nums[i];
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int takeLeft = nums[i] - solve(nums, i + 1, j, dp);
+        int takeRight = nums[j] - solve(nums, i, j - 1, dp);
+
+        return dp[i][j] = max(takeLeft, takeRight);
     }
+
     bool predictTheWinner(vector<int>& nums) {
-        int p1=0;
-        int p2=0;
-        int n=nums.size();
-        return dpFun(nums,0,n-1,p1,p2,true);
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return solve(nums, 0, n - 1, dp) >= 0;
     }
 };
