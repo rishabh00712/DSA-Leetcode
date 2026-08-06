b97ca496d3395aba03192f85cda2092979bbe88b
Rishabh Garai <160108941+rishabh00712@users.noreply.github.com>
2026-08-06 16:49:32 +0530

    Time: 7 ms (0.48%) | Memory: 8.7 MB (37.77%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isDivisible(int n,int t){
+        int product=1;
+        while(n>0){
+            product*=(n%10);
+            cout<<product<<endl;
+            n/=10;
+        }
+        return product%t==0 ? true : false;
+    }
+    int smallestNumber(int n, int t) {
+        for(int i=0;i<=10;i++){
+            if(isDivisible(n+i,t)){
+                return n+i;
+            }
+        }
+        return  -1;
+    }
+};
