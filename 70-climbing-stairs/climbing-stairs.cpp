09fd4bba73d3b662395ac6d3bb715a68b139effb
Rishabh Garai <160108941+rishabh00712@users.noreply.github.com>
2026-09-18 10:13:10 +0530

    Time: 3 ms (0.36%) | Memory: 8.6 MB (20.00%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int dpFun(int n,vector<int>& dp){
+        cout<<n<<endl;
+        if(n==0) return 1;
+        //if(n<0) return 0;
+        if(n==1) return 1;//as for 1 index we only have one opetion that jump just one and reach to index 0
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=dpFun(n-1,dp)+dpFun(n-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return dpFun(n,dp);
+    }
+};
