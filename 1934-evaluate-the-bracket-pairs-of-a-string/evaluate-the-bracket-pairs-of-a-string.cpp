5681aecec75788eacd62a1aa11dd61bfad6e9a85
Rishabh Garai <160108941+rishabh00712@users.noreply.github.com>
2026-09-26 11:02:02 +0530

    Time: 1268 ms (5.30%) | Memory: 500.3 MB (6.23%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string findKey(string s,int& i){
+        string key;
+        while(i<s.size() && s[i]!=')'){
+            key.push_back(s[i]);
+            i++;
+        }
+        cout<<key<<endl;
+        return key;
+    }
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string,string> KTVMap;//ket to value map
+        for(auto pair : knowledge){
+            string key=pair[0];
+            string val=pair[1];
+            KTVMap[key]=val;
+        }
+        int n=s.size();
+        string ans;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                i++;
+                string key=findKey(s,i);
+                string val="?";
+                if(KTVMap.find(key)!=KTVMap.end()){
+                    val=KTVMap[key];
+                }
+                ans+=val;
+            }else{
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
